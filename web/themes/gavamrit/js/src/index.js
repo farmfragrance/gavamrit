afdb9de9feccfce9596cab316b5de14d3cc01504
farmfragrance <gavamrit1@gmail.com>
2020-09-01 12:44:58 +0530

    Signed-off-by: farmfragrance <gavamrit1@gmail.com>

---
+++ web/themes/gavamrit/js/src/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ $("#layerslider").layerSlider({
+      pauseOnHover: false,
+      skin: 'borderlessdark3d',
+      hoverBottomNav: true,
+      skinsPath: '../layerslider/skins/'
+}); 
+
+});
