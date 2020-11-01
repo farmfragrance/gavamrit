a697c94ac696e67f07d4973a712388eb4d5b81a5
sainimani <gavamrit1@gmail.com>
2020-11-01 08:51:05 +0530

    del..

--- themes/gavamrit/js/src/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
- $("#layerslider").layerSlider({
-      pauseOnHover: false,
-      skin: 'borderlessdark3d',
-      hoverBottomNav: true,
-      skinsPath: '../layerslider/skins/'
-}); 
-
-});
